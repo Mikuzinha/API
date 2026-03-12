b13870f3a4dd4cdfe0fe00e7c21c0fa417b8c2b2
Rafael Gonçalves Saches <00001128348159SP@al.educacao.sp.gov.br>
2026-03-12 13:27:48 -0300

    meu projeto APi

---
+++ api-node/src/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function(req, res){
+    res.send("Hello World");
+});
+
+app.post('/pessoa', function(req, res){
+    console.log(req);
+    res.json({
+        statusCode: 200
+    });
+});
+
+app.listen(3000, () => console.log("Api Rodando."));
